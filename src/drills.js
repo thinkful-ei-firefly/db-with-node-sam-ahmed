41d4e99d2e92b8e5258c42dbf8e797e9bb5a7bfa
Ahmed <ahmed.max7586@gmail.com>
2019-08-27 12:38:16 -0400

    done drill 2 & 3

--- src/drills.js
+++ src/drills.js
@@ -1,16 +1,43 @@
-const knex = require('knex');
+const knex = require("knex");
 
 const db = knex({
-  client: 'pg',
-  connection: 'postgressql://dunder-mifflin@localhost/knex_practice'
-})
+  client: "pg",
+  connection: "postgressql://dunder-mifflin@localhost/knex_practice"
+});
 
-const getTextItems = (searchTerm) => {
-  db.select('*')
-    .from('shopping_list')
-    .where('name', 'ILIKE', `%${searchTerm}%`)
+const getTextItems = searchTerm => {
+  db.select("*")
+    .from("shopping_list")
+    .where("name", "ILIKE", `%${searchTerm}%`)
     .then(data => console.log(data))
-    .finally(()=>db.destroy());
+    .finally(() => db.destroy());
+};
+
+// getTextItems('burger');
+
+function paginateItems(pageNum) {
+  const itemsPerPage = 6;
+  const offset = itemsPerPage * (pageNum - 1);
+  db.select("*")
+    .from("shopping_list")
+    .limit(itemsPerPage)
+    .offset(offset)
+    .then(result => {
+      console.log(result);
+    })
+    .finally(() => db.destroy());
 }
 
-getTextItems('burger');
+// paginateItems(3);
+
+function addedAfter(daysAgo) {
+  const date = db.raw(`now() - '?? days':: INTERVAL`, daysAgo);
+  db.from("shopping_list")
+    .where("date_added", ">", date)
+    .then(result => {
+      console.log(result);
+    })
+    .finally(() => db.destroy());
+}
+
+addedAfter(5)
